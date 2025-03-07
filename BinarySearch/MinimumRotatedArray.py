0c721779c081b1d18a73a766b5f2662d5f446bff
Anirudh Arunkumar <aarunkumar8@gatech.edu>
2025-03-07 17:58:29 -0500

    rotated array search

---
+++ BinarySearch/MinimumRotatedArray.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+
+            mid = ((right - left) // 2) + left
+
+            if nums[mid] < nums[right]:
+                right = mid
+            else:
+                left = mid + 1
+
+
+        return nums[left]
